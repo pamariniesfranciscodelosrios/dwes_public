bedc74288d37d9b0d512c55085034dbca7728b51
pamariniesfranciscodelosrios <pamarin@iesfranciscodelosrios.es>
2026-02-01 17:25:15 +0100

    1/2/26

--- dwes2526_u6_556a5sd6a1sd65asd65as6d51a6sd/UD6-Laravel/Apuntes Gogodev/1_Laravel/sinTerminar/7_Seeder_Factory_Faker/databases/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-
-
-Route::get('/',[ProductController::class,'index'])->name('product.index');//Ruta principal
